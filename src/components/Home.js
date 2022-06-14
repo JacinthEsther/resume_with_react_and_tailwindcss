eb6301be7a3028f64a29f40c4437c01aac543e55
Agboniro Esther Jacinta <agbonirojacinta@gmail.com>
2022-06-14 17:22:24 +0100

    updated code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
+import { Link } from 'react-scroll';
 
 const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-pink-600'>Hi, my name is</p>
+        <p className='text-pink-600 mt-5'>Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
           Agboniro Esther Jacinta
         </h1>
@@ -20,7 +21,10 @@ const Home = () => {
         </p>
         <div>
           <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
-            View Work
+            
+            <Link to="work" >
+              View Work
+            </Link>
             <span className='group-hover:rotate-90 duration-300'>
               <HiArrowNarrowRight className='ml-3 ' />
             </span>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -124,7 +124,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='/https://docs.google.com/document/d/1JPPm4tg78j0cV-1r9CYBoySSfJWUg4qD33AZWcqTGQ4/edit?usp=sharing'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
